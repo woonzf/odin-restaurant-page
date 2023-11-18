548861510f65921583be3f76dd1948f02496ac38
woonzf <137765412+woonzf@users.noreply.github.com>
2023-11-18 19:52:59 +0800

    Add Header, Content and Footer with CSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
